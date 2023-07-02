bfc684afb86970e1789ada78ffe989c65dd10b7c
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-07-02 05:29:55 -0700

    Circle card works

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 var hederaRoutes = require('./hedera') // import Hedera routes
+var circleRoutes = require('./circle') // import Circle routes
 
 // Middleware
 // Add cors
@@ -16,5 +17,6 @@ router.use(
 router.use(express.json()) // for parsing application/json
 
 router.use('/hedera', hederaRoutes) // use Hedera routes for paths starting with /hedera
+router.use('/circle', circleRoutes) // use Circle routes for paths starting with /circle
 
 module.exports = router
